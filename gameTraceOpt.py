a2c5f89af97c7c0ccf8397812807417747311792
Ray-Leonard <rayleonard726@gmail.com>
2021-11-15 06:34:51 -0500

    ingametrrace done

--- gameTraceOpt.py
+++ gameTraceOpt.py
@@ -1,7 +1,7 @@
 import lineemup
 
 
-def inGameTrace(f, move, newBoardGraph, evalTime, numOfStates, depthOfNodes):
+def inGameTrace(f, curr_player, move, newBoardGraph, evalTime, numOfStates, depthOfNodes, ard, count):
     """Responsible for displaying in loop game trace."""
     """
        Imported Parameters
@@ -30,18 +30,16 @@ def inGameTrace(f, move, newBoardGraph, evalTime, numOfStates, depthOfNodes):
     averageDepth = sum(depthOfNodes) / len(depthOfNodes)
 
     # write
-    f.write("5(a). The move taken: {}\n\r".format(move))
-    f.write("5(b). New configuration of the board.{}\n".format(newBoardGraph))
+    f.write("5(a). {} plays: {}\n\r".format(curr_player, move))
+    f.write("5(b). (move {}) \nNew configuration of the board.{}\n".format(count, newBoardGraph))
     f.write("5(ci). The evaluation time of the heuristic: {}s\n".format(evalTime))
     f.write("5(cii). The number of states evaluated by the heuristic function: {}\n".format(numOfStates))
     f.write("5(ciii). The number of states evaluated at each depth: {}\n".format(statesOfEachDepth))
     f.write("5(civ).The average depth (AD): {}\n".format(averageDepth))
-    # Todo 5(cv). The average recursion depth (ARD) at the current state
-    f.write("5(cv). The average recursion depth (ARD): {}\n\r".format(" "))
-    # need to write a recursive function to grab the information
+    f.write("5(cv). The average recursion depth (ARD): {}\n\r".format(ard))
 
 
-def endGameTrace(f, winner, avgEvalTime, totalEval, avgAvgDepth, statesEvalAtDepth, avgARD, totalMove):
+def endGameTrace(f, winner, totalEvalTime, totalEval, depthOfNodes, avgARDList, totalMove):
     """Responsible for displaying end game statics."""
     """
            Imported Parameters
@@ -62,86 +60,26 @@ def endGameTrace(f, winner, avgEvalTime, totalEval, avgAvgDepth, statesEvalAtDep
                 totalMove:
                     The total number of moves in the game
     """
+
+    avgEvalTime = totalEvalTime / totalMove
+    statesEvalAtDepth = ["depth{} = {}".format(x, depthOfNodes.count(x)) for x in sorted(set(depthOfNodes))]
+    avgAvgDepth = sum(depthOfNodes) / len(depthOfNodes)
+    avgARD = sum(avgARDList) / len(avgARDList)
+
     # print
-    f.write("6(a). The Winner: {}\n".format(winner))
+    if winner != '.':
+        f.write("6(a). The Winner: {}\n".format(winner))
+    else:
+        f.write("6(a). Tie\n")
+
+
     f.write("6(bi). Average evaluation time: {}\n".format(avgEvalTime))
+
     f.write("6(bii). Total heuristic evaluations: {}\n".format(totalEval))
+
     f.write("6(biii). Average of Average depth: {}\n".format(avgAvgDepth))
     f.write("6(biv). The total number of states evaluated at each depth: {}\n".format(statesEvalAtDepth))
     f.write("6(bv). Average of average recursion depth: {}\n".format(avgARD))
     f.write("6(bvi). The total number of moves in the game: {}\n\r".format(totalMove))
 
 
-def main():
-    # sample parameters
-    n = 1
-    b = 2
-    s = 3
-    t = 4
-    bloc = [(2, 3), (3, 4), (4, 5)]
-    # Player 1:
-    playerType1 = "AI"
-    depth1 = 5
-    a1 = True
-    heuristicFunc1 = "manhattan"
-    # Player 2:
-    playerType2 = "AI"
-    depth2 = 5
-    a2 = True
-    heuristicFunc2 = "manhattan"
-
-    # inGame sample variable
-    _move = ["B", 4]
-    _evalTime = 10
-    _numOfStates = 9999
-    _depthOfNodes = [3, 3, 2, 3, 3, 2, 2, 1, 1]
-
-    # endGame sample variable
-    _winner = "AI-1"
-    _avgEvalTime = 5
-    _totalEval = 9999999
-    _avgAvgDepth = 123456789
-    _statesEvalAtDepth = {4: 22582, 8: 175396, 7: 590, 6: 663, 5: 85744, 3: 88, 2: 20, 1: 2}
-    _avgARD = 8
-    _totalMove = 999999999999
-
-    #sample graph
-    g = lineemup.Game(n=5, s=3, b=0)
-    g1 = lineemup.Game(n=5, s=3, b=0)
-    g.current_state = [
-        ['X', '.', '.', '.', '.'],
-        ['O', 'X', 'X', '.', 'O'],
-        ['X', 'O', '.', 'O', '.'],
-        ['X', 'X', 'X', 'O', 'O'],
-        ['.', 'X', '.', 'X', '.']
-    ]
-    g1.current_state = [
-        ['X', '.', 'O', 'O', 'O'],
-        ['O', 'X', 'X', '.', 'O'],
-        ['X', 'O', 'O', 'O', 'O'],
-        ['X', 'X', 'X', 'O', 'O'],
-        ['O', 'X', '.', 'X', '.']
-    ]
-    oldBoardGraph = g.draw_board()
-    newBoardGraph = g1.draw_board()
-
-    # start writing
-    fileFormat = "gameTrace-{}{}{}{}.txt".format(n, b, s, t)
-    fWriter = open(fileFormat, "w")
-
-    # 1. The parameters of the game: the values of n, b, s, t
-    fWriter.write("Game parameters: [Size n = {}, Blocs b = {}, winSize s = {}, Overtime t = {}]\n".format(n, b, s, t))
-    # 2. The position of the blocs
-    fWriter.write("Position of each blocs: {}\n\r".format(bloc))
-    # 3. player info
-    fWriter.write("Player 1: {}, d={}, a={}, {}\n".format(playerType1, depth1, a1, heuristicFunc1))
-    fWriter.write("Player 2: {}, d={}, a={}, {}\n\r".format(playerType2, depth2, a2, heuristicFunc2))
-
-    inGameTrace(fWriter, oldBoardGraph, _move, newBoardGraph, _evalTime, _numOfStates, _depthOfNodes)
-    endGameTrace(fWriter, _winner, _avgEvalTime, _totalEval, _avgAvgDepth, _statesEvalAtDepth, _avgARD, _totalMove)
-
-    fWriter.close()
-
-
-if __name__ == "__main__":
-    main()
